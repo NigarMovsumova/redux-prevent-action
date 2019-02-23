575221d286fa74aeab9535866b1bbb543ad41f8e
Nigar <movsum.nigar@gmail.com>
2019-02-24 01:15:22 +0400

    added index.js

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import {createStore, applyMiddleware} from 'redux';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+import App from './components/App';
+import reducers from './reducers';
+
+const preventActionMiddleWare = store=> next=> action=>{
+
+    if (store.getState().counter%2==0){
+        let mutatedAction= action;
+        console.log('type= '+mutatedAction.type);
+        mutatedAction.type='IGNORED_ACTION';
+        next(mutatedAction);
+    }
+    else {
+        next(action);
+    }
+    console.log("Store condition after action: ");
+    console.log(store.getState());
+    console.log('\n');
+    console.log("Middleware triggered this action:", action.type);
+    //+1 is for initial condition
+    console.log("Count of dispatched actions at " + new Date() + " = "+ (store.getState().counter))
+    console.log('\n');
+}
+
+
+
+const store = createStore(reducers, applyMiddleware(thunk, preventActionMiddleWare));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.querySelector("#root")
+);
