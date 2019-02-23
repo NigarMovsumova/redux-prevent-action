274a3d042b3ee13ab2c6ba4cfa60f62c8e1c5778
Nigar <movsum.nigar@gmail.com>
2019-02-24 01:27:37 +0400

    added posts/users/counter reducers

---
+++ src/reducers/counterReducer.jsx
@@ -0,0 +1,4 @@
+
+export const counterReducer = (state = 0, action) => {
+    return  ++state;
+}
---
+++ src/reducers/usersReducer.jsx
@@ -0,0 +1,6 @@
+export const usersReducer= (state=[], action)=>{
+    switch (action.type){
+        case 'FETCH_USERS':return action.payload;
+        default : return state;
+    }
+}
